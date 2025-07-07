257e46075d181a913437a73982f1e0fd8b304887
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-07-07 10:15:02 +0530

    update

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,24 +1,23 @@
-import express from 'express'
+import express from "express";
 // import tryCatch from './utils/trycatch.js';
 // import Register from '../controller/auth/registerController.js';
 // import { sendOTP, verifyOTP } from '../controller/auth/loginController.js';
-import Register from '../controller/auth/registerController.js';
-import { sendOTP, verifyOTP } from '../controller/auth/loginController.js';
-import tryCatch from '../utils/trycatch.js';
-import { loginWithPass } from '../controller/auth/loginWithPass.js';
-import adminLogout from '../controller/auth/logout.js';
+import Register from "../controller/auth/registerController.js";
+import { sendOTP, verifyOTP } from "../controller/auth/loginController.js";
+import tryCatch from "../utils/trycatch.js";
+import { loginWithPass } from "../controller/auth/loginWithPass.js";
+import adminLogout from "../controller/auth/logout.js";
 
-const authRouter =express.Router();
+const authRouter = express.Router();
 
 authRouter
-//emailpassword register
-.post('/register',tryCatch(Register))
+  //emailpassword register
+  .post("/register", tryCatch(Register))
 
+  //googleregister
+  .post("/sendOTP", tryCatch(sendOTP))
+  .post("/verifyOTP", tryCatch(verifyOTP))
+  .post("/loginWithPass", tryCatch(loginWithPass))
+  .post("/logout", tryCatch(adminLogout));
 
-//googleregister
-.post('/sendOTP',tryCatch(sendOTP))
-.post('/verifyOTP',tryCatch(verifyOTP))
-.post('/loginWithPass',tryCatch(loginWithPass))
-.post('/logout', tryCatch(adminLogout))
-
-export default authRouter
+export default authRouter;
