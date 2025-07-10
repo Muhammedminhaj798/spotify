91c9d5fab2edd93e62fb84630aedf0063591a8be
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-07-10 15:34:30 +0530

    Update AxiosInstance.js

--- frontend/src/AxiosInstance.js
+++ frontend/src/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // AxiosInstance.js
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL: process.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
