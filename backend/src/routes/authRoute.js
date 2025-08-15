6baa09316d6ee7ae766591b4ce1339971d60421f
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-08-15 17:10:47 +0530

    udpate

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,7 +1,4 @@
 import express from "express";
-// import tryCatch from './utils/trycatch.js';
-// import Register from '../controller/auth/registerController.js';
-// import { sendOTP, verifyOTP } from '../controller/auth/loginController.js';
 import Register from "../controller/auth/registerController.js";
 import { sendOTP, verifyOTP } from "../controller/auth/loginController.js";
 import tryCatch from "../utils/trycatch.js";
@@ -11,10 +8,7 @@ import adminLogout from "../controller/auth/logout.js";
 const authRouter = express.Router();
 
 authRouter
-  //emailpassword register
   .post("/register", tryCatch(Register))
-
-  //googleregister
   .post("/sendOTP", tryCatch(sendOTP))
   .post("/verifyOTP", tryCatch(verifyOTP))
   .post("/loginWithPass", tryCatch(loginWithPass))
