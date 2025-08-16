ccfb02a9043882c9571c769cd65b4c2a43b2b52e
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-08-16 23:05:57 +0530

    update

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import tryCatch from "../utils/trycatch.js";
-import addPlaylist, {  addSongPlaylist, deletePlaylist, getPlaylists, playlistById, removeSongPlaylist } from "../controller/user/playlistController.js";
+import addPlaylist, { addSongPlaylist, deletePlaylist, getPlaylists, playlistById, removeSongPlaylist } from "../controller/user/playlistController.js";
 import user_auth from "../middleware/authmiddleWare.js";
 import upload from "../middleware/uploadMiddleware.js";
 import { getSearchController } from "../controller/user/searchController.js";
@@ -10,13 +10,13 @@ const userRoutes = express.Router();
 
 userRoutes
     .post("/addPlaylist", user_auth, tryCatch(addPlaylist))
-    .get("/getPlaylist",user_auth,tryCatch(getPlaylists))
-    .get("getAllPlaylist",tryCatch(deletePlaylist))
-    .get('/playlistById/:id',user_auth,tryCatch(playlistById))
-    .post('/addSongPlaylist/:id', user_auth,tryCatch(addSongPlaylist))
-    .delete('/removeSongPlaylist/:id', user_auth,tryCatch(removeSongPlaylist))
-    .get('/searchBar',tryCatch(getSearchController))
-    .get('/getArtistSongById/:id',tryCatch(getArtistSongById))
-    .delete("/deletePlaylist/:id",user_auth,tryCatch(deletePlaylist))
+    .get("/getPlaylist", user_auth, tryCatch(getPlaylists))
+    .get("getAllPlaylist", tryCatch(deletePlaylist))
+    .get('/playlistById/:id', user_auth, tryCatch(playlistById))
+    .post('/addSongPlaylist/:id', user_auth, tryCatch(addSongPlaylist))
+    .delete('/removeSongPlaylist/:id', user_auth, tryCatch(removeSongPlaylist))
+    .get('/searchBar', tryCatch(getSearchController))
+    .get('/getArtistSongById/:id', tryCatch(getArtistSongById))
+    .delete("/deletePlaylist/:id", user_auth, tryCatch(deletePlaylist))
 
 export default userRoutes;
