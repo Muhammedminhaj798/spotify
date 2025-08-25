c985d83c00ae778a8df1a0046643bf02145045ce
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-08-25 20:54:08 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,6 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-
-
 app.use("/api/auth", authRouter);
 app.use('/api/admin', adminRoutes)
 app.use('/api/user', userRoutes)
